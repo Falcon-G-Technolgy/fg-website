b6719afe63a121b1e286b67abe2677ab31f43a17
isaaconek <onekdev5@gmail.com>
2025-01-05 09:24:48 +0100

    Premier commit, Initialisation du projet avec vite; Installation de tailwind css et configuration de celui-ci

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-yellow-500 text-3xl font-bold text-center">Bienvenue sur ce nouveau projet</h1>
+    </>
+  )
+}
+
+export default App
