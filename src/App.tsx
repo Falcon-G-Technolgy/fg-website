975a9bdc6352d5d7ca20a00c55fb8cb7c88eff05
isaaconek <onekdev5@gmail.com>
2025-01-07 14:59:47 +0100

    Ajout des breakpoint personalise pour gerer le responsive design, dans le fichier de configuration de tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <>
-      <h1 className="text-yellow-500 text-3xl font-bold text-center">Bienvenue sur ce nouveau projet</h1>
+      <h1 className="xxs:text-red-500 xs:text-green-800 tablet:text-yellow-500 desktop:text-orange-600 text-3xl font-bold text-center">Bienvenue sur ce nouveau projet</h1>
     </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'xxs': '230px', // Très petit téléphone
+        'xs': '300px', // Smartphone classique ou un peu grand
+        'tablet': '768px', // Tablette
+        'desktop': '1024px', // Ordinateur
+      },
+    },
   },
   plugins: [],
 }
