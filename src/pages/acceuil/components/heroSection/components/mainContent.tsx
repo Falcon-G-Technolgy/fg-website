339b954f543a0f519977503c635e1b2d242d0bd5
isaaconek <onekdev5@gmail.com>
2025-01-14 17:09:31 +0100

    Refactorisation de la section hero

--- src/pages/acceuil/components/heroSection/components/mainContent.tsx
+++ src/pages/acceuil/components/heroSection/components/mainContent.tsx
@@ -14,11 +14,11 @@ const MainContent = () => {
             <div className="mb-4 flex flex-col gap-3">
                 <motion.h1
                 initial={{opacity: 0, x: -20}} animate={{opacity: 1, x: 0, transition: {delay: 1}}}
-                className=" text-4xl leading-tight sm:text-[2.5rem] font-bold sm:w-[50rem] mb-2">Envolez-vous dans l’univers technologique et digital pour booster votre <strong className="text-white bg-black px-4">business</strong> !</motion.h1>
+                className=" text-[2.5rem] leading-tight sm:text-[3rem] font-bold sm:w-[50rem] mb-2 text-black">Envolez-vous dans l’univers technologique et digital pour booster votre <strong className="text-white bg-black px-4">business</strong> !</motion.h1>
                 
                 <motion.h2 
                 initial={{opacity: 0, x: -20}} animate={{opacity: 1, x: 0, transition: {delay: 1.5}}}
-                className="text-2xl font-semibold text-main-black mb-2">Alors, prêt à embarquer ?</motion.h2>                
+                className="text-2xl font-medium text-main-black mb-2 opacity-85">Alors, prêt à embarquer ?</motion.h2>                
             </div>
 
             <motion.ul
@@ -32,7 +32,7 @@ const MainContent = () => {
             initial={{ opacity: 0, y: -20, scale: 0.5 }} animate={{ opacity: 1, y: 0,scale:1 , transition: {delay: 2.5}}}
             className="text-white relative top-7 flex flex-col sm:flex-row gap-7  sm:gap-3 z-10 ">
                 <MainCTA to="" linkText="Commander un logiciel" bgColor="black"/>
-                <MainCTA to="" linkText="Nous contacter" bgColor="blue-900"/>
+                <MainCTA to="" linkText="Nous contacter" bgColor="main-blue"/>
             </motion.div>
         </div>
     );
--- src/pages/acceuil/components/heroSection/heroSection.tsx
+++ src/pages/acceuil/components/heroSection/heroSection.tsx
@@ -4,7 +4,7 @@ import MainContent from "./components/mainContent";
 const LazySectionImage = React.lazy (() => import ('./components/sectionImage'))
 const HeroSection = () => {
     return (
-        <section className="bg-main-yellow sm:h-[36rem] flex items-center sm:justify-between flex-col sm:flex-row pt-16 sm:pt-0 ">
+        <section className="bg-main-yellow sm:h-[36rem] flex items-center sm:justify-between flex-col sm:flex-row  pt-24 sm:pt-0 ">
             <MainContent/>
             <LazySectionImage/>
         </section>
