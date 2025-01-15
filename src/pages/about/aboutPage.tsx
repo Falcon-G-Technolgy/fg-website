3f758e1221dfd94fcd7e2776469c0614d22db395
jacques onek <onekongajaque@gmail.com>
2025-01-15 09:39:03 +0100

    je vien de modifier le header en placean tle nav et la section header a l'interieure d'un div

--- src/pages/about/aboutPage.tsx
+++ src/pages/about/aboutPage.tsx
@@ -4,8 +4,11 @@ import Header from "./component/Header";
 const AboutPage = () => {
     return (
         <div className="flex flex-col justify-around overflow-x-hidden">
+            <div className="flex flex-col justify-between">
             <Navbar/>
             <Header/>
+            </div>
+           
         </div>
     );
 }
