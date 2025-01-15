c21513a136d8502eb50070dc4508e2897190a65a
isaaconek <onekdev5@gmail.com>
2025-01-15 08:35:08 +0100

    Resolution des erreurs de build

--- src/pages/acceuil/components/section-3/components/itemList.tsx
+++ src/pages/acceuil/components/section-3/components/itemList.tsx
@@ -6,11 +6,11 @@ type ItemType = {
     textMarked: string,
     color?: string
 }
-const ItemList = ({ firstText, textMarked, color = 'white' }: ItemType) => {
+const ItemList = ({ firstText, textMarked }: ItemType) => {
     return (
         <li className="flex gap-2 ">
             <div className="w-5"><ListPoint/></div>
-            <p className="text-black opacity-90 flex items-center gap-1 "> {firstText} <TextMarked text={textMarked} color={color} bg="black" /> </p>
+            <p className="text-black opacity-90 flex items-center gap-1 "> {firstText} <TextMarked text={textMarked} /> </p>
         </li>
     );
 }
