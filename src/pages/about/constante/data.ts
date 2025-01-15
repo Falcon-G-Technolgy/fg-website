653d18d4ec8dcaf316d666d2232067ca6df19494
jacques onek <onekongajaque@gmail.com>
2025-01-15 09:41:14 +0100

    jevien de cree le dossier desconstante de text que je vais utilisez dans la page a propos

---
+++ src/pages/about/constante/data.ts
@@ -0,0 +1,7 @@
+
+
+export const aboutData = {
+    titre : "Boostez votre bussness grace a la ",
+    span:"technologie ",
+    description :"Bienvenue chez Falcong Technology , votre partenaire de choix en matiere du digital ",
+} 
