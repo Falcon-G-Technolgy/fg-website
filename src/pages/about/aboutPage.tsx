34b2427686e046bca08f176bd092004a725ff15a
jacques onek <onekongajaque@gmail.com>
2025-01-15 07:17:32 +0100

    creation du header dans la page about

--- src/pages/about/aboutPage.tsx
+++ src/pages/about/aboutPage.tsx
@@ -1,7 +1,11 @@
+import Navbar from "../../components/navBar/navbar";
+import Header from "./component/Header";
+
 const AboutPage = () => {
     return (
         <div>
-            
+            <Navbar/>
+            <Header/>
         </div>
     );
 }
