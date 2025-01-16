7a86a86e0bad992382b9969e274a9ad67256d2ef
jacques onek <onekongajaque@gmail.com>
2025-01-16 17:57:24 +0100

    je cree la contstante pour la section QuiSommeNous

--- src/pages/about/constante/data.ts
+++ src/pages/about/constante/data.ts
@@ -4,4 +4,14 @@ export const aboutData = {
     titre : "Boostez votre bussness grace a la ",
     span:"technologie ",
     description :"Bienvenue chez Falcong Technology , votre partenaire de choix en matiere du digital ",
-} 
+}
+
+export const aboutUs = {
+    p1:`  FalconG Technologie est une agence des services
+     numerique specialisee dans la conception et le developpement des logiciels ( application mobiles , Web et Desktop), 
+     ainsi que la conception des support marketing tel que des affiches publicitaires , des branding ou encore des 
+     videos promotionnelles .  `,
+    p2:` Depuis 2021 , notre equipe , situee a kinshasa en Republique 
+    democratique du congo , travaille avec des imprimeries,des pharmacie et diverses entreprises desirant adopterdes solution numerique pour optimiser leur productivite et 
+    offrir un meilleur  service a leur clientele`
+}
