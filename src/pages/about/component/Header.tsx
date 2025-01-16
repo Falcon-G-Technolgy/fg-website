f01bc31effc00a2cb2f74ceeb22d4952acb19432
jacques onek <onekongajaque@gmail.com>
2025-01-16 16:32:39 +0100

    je finalise la section header sur desktop

--- src/pages/about/component/Header.tsx
+++ src/pages/about/component/Header.tsx
@@ -1,25 +1,28 @@
-import { div, title } from "framer-motion/client"
 import { aboutData } from "../constante/data"
 
 const {titre,span,description} = aboutData
 const Header = () => {
   return (
-    <div className='relative flex w-screen h-screen px-16 pt-24 mt-10 bg_about'>
-        <div className="flex flex-col text-white ">
-            <div className="flex flex-col">
+    <div className='relative flex w-screen h-screen justify-between bg_about pt-24 px-20 '>
+        <div className="flex flex-col w-6/12 text-white mt-10 pt-14 lg:w-7/12 lg:mt-2 max-lg:w-full" >
+            <div className="flex flex-col max-w-6xl mt-10">
                 <h1 className="text-5xl font-bold text-white">{titre}</h1>
-                <div>
-                 <p className="bg-[#FBA200] rotate-3 w-3/6 py-2 px-2">
-                    <span className="text-5xl rotate-0">{span}</span>
+                <div className="flex my-3 text-3xl items-center">
+                 <p className="bg-[#FBA200] rotate-1 w-auto py-1 px-1 mr-2 ">
+                    <span className="text-4xl -rotate-3 text-black font-bold">{span}</span> 
                 </p>
-                !    
+                   <span className="font-medium text-3xl ">!</span>
                 </div>
-                
+                 
             </div>
-            <p>
-                {description}
-            </p>
+                <p className="text-xl mt-4">
+                  {description}
+                </p>
         </div>
+        <div className="relative size-[36%]">
+        <img src="../../hero-about-page.png" alt="" className="object-cover" />
+        </div>
+        
     </div>
   )
 }
