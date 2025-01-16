cd342d47116a6363d7843dabd90dfd091665e59d
jacques onek <onekongajaque@gmail.com>
2025-01-16 18:44:17 +0100

    je vien de copie un bout de code pour le mettre dans mision.tsx

--- src/pages/about/component/Header.tsx
+++ src/pages/about/component/Header.tsx
@@ -5,11 +5,11 @@ const {titre,span,description} = aboutData
 const Header = () => {
   return (
     
-    <div className='relative flex  w-screen h-screen justify-between bg_about pt-24 px-20 max-md:flex-col '>
-       <div className="flex flex-col w-7/12 text-white mt-10 pt-14 lg:w-7/12 lg:mt-2 md:w-full" >
+    <div className='relative flex  w-screen h-screen justify-between bg_about pt-24 px-20 max-md:flex-col  '>
+       <div className="flex flex-col w-7/12 text-white xl:w-6/12 mt-10 pt-14 lg:w-7/12 lg:mt-2 md:w-full" >
            <div className="flex flex-col max-w-6xl mt-10">
-               <h1 className="text-5xl font-bold text-white">{titre}</h1>
-               <div className="flex my-3 text-3xl items-center">
+               <h1 className="text-6xl font-bold text-white lg:text-5xl" >{titre}</h1>
+               <div className="flex my-4 text-3xl items-center">
                   <p className="bg-[#FBA200] rotate-1 w-auto py-1 px-1 mr-2 ">
                        <span className="text-4xl -rotate-3 text-black font-bold">{span}</span>
                   </p>
