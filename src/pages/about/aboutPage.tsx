0b9eca9e35f1a6f6429d4466091915795a32ab81
jacques onek <onekongajaque@gmail.com>
2025-01-16 16:35:00 +0100

    je revois le fichier about.tsx

--- src/pages/about/aboutPage.tsx
+++ src/pages/about/aboutPage.tsx
@@ -1,12 +1,14 @@
 import Navbar from "../../components/navBar/navbar";
 import Header from "./component/Header";
+import QuiNousSomme from "./component/QuiNousSomme";
 
 const AboutPage = () => {
     return (
         <div className="flex flex-col justify-around overflow-x-hidden">
-            <div className="flex flex-col justify-between">
+            <div className="flex flex-col justify-between gap-3">
             <Navbar/>
             <Header/>
+            <QuiNousSomme/>
             </div>
            
         </div>
