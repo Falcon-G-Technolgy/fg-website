e94841090808c1585dd458b765d47b30d19a07b6
isaaconek <onekdev5@gmail.com>
2025-01-20 07:41:25 +0100

    Finission du footer avec l'ajout des liens utiles

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -28,28 +28,47 @@ const socialMediaTable = [
 
 const Footer: React.FC = () => {
     return (
-        <footer className="bg-main-black flex xs:items-center xs:justify-center h-40 xs:h-52 relative top-24 xs:top-20 px-8 py-5 text-main-white ">
-            <div className="flex flex-col items-end  xs:gap-3">
-                <div className="flex xs:flex-col items-center gap-2">
-                    <img src="/logotype-fg.PNG" className="size-16" alt="Logotype FG tech" />
-                    <div className="xs:text-center">
-                        <h1 className="font-bold">Falcon-G Technology</h1>
-                        <p>Se booster grace au digital !</p>
+        <footer className="bg-main-black flex flex-col gap-2  h-40 xs:h-auto relative top-24 tablet:top-10 xs:top-20 px-8 py-5 text-main-white ">
+            <div className="flex xs:flex-col xs:gap-12 xs:items-center justify-between xs:justify-center">
+                <div className="flex flex-col items-end  xs:gap-3">
+                    <div className="flex xs:flex-col items-center gap-2">
+                        <img src="/logotype-fg.PNG" className="size-16" alt="Logotype FG tech" />
+                        <div className="xs:text-center">
+                            <h1 className="font-bold">Falcon-G Technology</h1>
+                            <p>Se booster grace au digital !</p>
+                        </div>
+                    </div>
+                    <div className="flex gap-3 text-2xl relative right-9 xs:right-3 ">
+                        {
+                            socialMediaTable.map ((media, key) => (
+                                <Link key={key} to={media.link}>
+                                    <span className="hover:text-main-yellow">
+                                        { media.icon}
+                                    </span>
+                                </Link>
+                            ))
+                        }
                     </div>
                 </div>
-                <div className="flex gap-3 text-2xl relative right-9 xs:right-3 ">
-                    {
-                        socialMediaTable.map ((media, key) => (
-                            <Link key={key} to={media.link}>
-                                { media.icon}
-                            </Link>
-                        ))
-                    }
-                </div>
+                <div className="flex flex-col xs:text-center">
+                    <Link to="/a-propos">
+                        <span className="hover:text-main-yellow">À propos de nous</span>
+                    </Link>
+                    <Link to="/realisations">
+                        <span className="hover:text-main-yellow">Réalisations</span>
+                    </Link>
+                    <Link to="/expertises">
+                        <span className="hover:text-main-yellow">Expertise et services</span>
+                    </Link>
+                    <Link to="/blog">
+                        <span className="hover:text-main-yellow">Blog</span>
+                    </Link>
+                    <Link to="/contact">
+                        <span className="hover:text-main-yellow">Contact</span>
+                    </Link>
+                </div>                
             </div>
-            <div>
-
-            </div>
+            <hr />
         </footer>
     );
 }
