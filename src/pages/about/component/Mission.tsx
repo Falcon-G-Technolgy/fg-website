8ed09c1b6fd896d2cb2d1184a3665668d3cc3256
isaaconek <onekdev5@gmail.com>
2025-01-20 14:00:11 +0100

    Finnission de la page about

--- src/pages/about/component/Mission.tsx
+++ src/pages/about/component/Mission.tsx
@@ -5,7 +5,7 @@ const Mission = () => {
   return (
     <div className="px-10 mt-5 flex flex-col items-center gap-10">
         <div>
-          <TextMarked text="Quelle est Notre mission ?" style="px-2 py-1 bg-main-yellow text-2xl text-black font-medium "/>
+          <TextMarked text="Quelle est Notre mission ?" style="px-2 desktop:px-5 py-1 bg-main-yellow text-2xl text-black font-medium "/>
         </div>
         <div className="flex sm:justify-center sm:gap-16 xs:flex-col xs:items-center xs:gap-5">
           <img src="/images/about-page/about-3.png" loading="lazy" className="w-60" />
